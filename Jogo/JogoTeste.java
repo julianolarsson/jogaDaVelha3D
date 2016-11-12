548ad37973a8abb30f043625f1ec061afc1ae1c2
Juliano Larsson <juliano_larsson@hotmail.com>
2016-11-12 17:03:09 -0200

    dfadfads

---
+++ Jogo/JogoTeste.java
@@ -0,0 +1,33 @@
+
+/**
+ * Escreva a descrição da classe JogoTeste aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class JogoTeste
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * COnstrutor para objetos da classe JogoTeste
+     */
+    public JogoTeste()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Exemplo de método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   exemplo de um parâmetro de método
+     * @return     a soma de x com y 
+     */
+    public int sampleMethod(int y)
+    {
+        // ponha seu código aqui
+        return x + y;
+    }
+}
